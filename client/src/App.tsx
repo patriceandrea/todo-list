20d532ccda6fae2a13c53c032d693897b0996e87
patriceandrea <andrea_pavico98@yahoo.ca>
2022-06-12 15:31:54 -0400

    clears out tasks once task added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,7 +18,9 @@ const App: FC = () => {
   const addTask = (): void => {
     const newTask = { taskName: task, deadline: deadline };
     setTodoList([...todoList, newTask]);
-    console.log(todoList);
+    // clears out task once it was added  
+    setTask("");
+    setDeadline(0);
   };
 
 
@@ -30,11 +32,14 @@ const App: FC = () => {
             type="text"
             placeholder="Task..."
             name="task"
+            value={task}
             onChange={handleChange}
           />
           <input
             type="number"
             placeholder="Deadline (in Days)..."
+            name="deadline"
+            value={deadline}
             onChange={handleChange}
           />
         </div>
